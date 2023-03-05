a15f2ac5ae5f69b4085d4c4b9da8b44b9c7227d7
atjager <jageratti2000@gmail.com>
2023-03-05 20:54:59 +0100

    Initial commit

---
+++ backend/src/main/java/com/aj/carserviceticketing/CarServiceTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.aj.carserviceticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceTicketingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/aj/carserviceticketing/CarServiceTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.carserviceticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
